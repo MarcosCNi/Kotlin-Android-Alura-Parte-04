dfaccb27eb5bf175c57ee3746ee539dd62d97e77
Marcos <marcoscn11@hotmail.com>
2023-04-03 16:52:54 -0300

    module 3

--- app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
@@ -2,11 +2,23 @@ package br.com.alura.orgs.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import br.com.alura.orgs.model.Usuario
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UsuarioDao {
 
     @Insert
     suspend fun salvaUsuario(usuario: Usuario)
+
+    @Query("""
+        SELECT * FROM Usuario 
+        WHERE id=:usuarioId 
+        AND senha =:senha
+        """)
+    suspend fun autentica(usuarioId: String, senha: String): Usuario?
+
+    @Query("SELECT * FROM usuario WHERE id =:usuarioId")
+    fun buscaUsuarioPorId(usuarioId: String): Flow<Usuario>
 }
